8d6e436d6187941d60f91bbfb9e92ded8eb2fc4d
Daiyin0101 <146990966+Daiyin0101@users.noreply.github.com>
2024-04-24 14:51:51 -0700

    Delete Final_Project directory
    wrong project name

--- Final_Project/src/HealthCondition.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public enum HealthCondition {
-    HEALTHY,
-    SICK,
-    SICKWITHINFECTION,
-}
--- Final_Project/src/Location.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public enum Location {
-    REDMOND,
-    BELLEVUE,
-    KIRKLAND,
-    SEATTLE,
-}
